aa41647c08f86b09c1535b89dafae5f4f55c72a1
Agotia-Center-President-Milky <91369170+Agotia-Center-President-Milky@users.noreply.github.com>
2021-10-30 18:57:54 +0300

    console.js

---
+++ Modules/console.js
@@ -0,0 +1,16 @@
+const database = require("./db.json")
+const log1 = database.database
+const log2 = database.databaseJSON
+const log3 = database.license
+const log4 = database.lase
+const log5 = database.config
+const log6 = database.PasificLocation
+const log7 = database.ConfigData
+const log8 = database.Config.ConfigData
+const log9 = database.Config.Settings
+const log10 = database.Config.prototype.setup
+const log11 = database.Config.prototype.$schema
+var list = [log1, log2, log3, log4, log5, log6, log7, log8, log9, log10, log11];
+
+
+console.log(list);
